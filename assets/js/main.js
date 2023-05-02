38c57a175e8df178045e9aa59935053791451a95
warnerb47 <taphagaye2001.mg@gmail.com>
2023-05-02 12:24:29 +0000

    feat: add hero wave

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,28 +26,6 @@ window.onscroll =  () => {
         navbarTogglerNine.classList.toggle("active");
       });
   
-      // ==== left sidebar toggle
-      let sidebarLeft = document.querySelector(".sidebar-left");
-      let overlayLeft = document.querySelector(".overlay-left");
-      let sidebarClose = document.querySelector(".sidebar-close .close");
-  
-      overlayLeft.addEventListener("click", function () {
-        sidebarLeft.classList.toggle("open");
-        overlayLeft.classList.toggle("open");
-      });
-      sidebarClose.addEventListener("click", function () {
-        sidebarLeft.classList.remove("open");
-        overlayLeft.classList.remove("open");
-      });
-  
-      // ===== navbar nine sideMenu
-    //   let sideMenuLeftNine = document.querySelector(".navbar-nine .menu-bar");
-  
-    //   sideMenuLeftNine.addEventListener("click", function () {
-    //     sidebarLeft.classList.add("open");
-    //     overlayLeft.classList.add("open");
-    //   });
-  
       //========= glightbox
       GLightbox({
         'href': 'https://www.youtube.com/watch?v=r44RKWyfcFw&fbclid=IwAR21beSJORalzmzokxDRcGfkZA1AtRTE__l5N4r09HcGS5Y6vOluyouM9EM',
